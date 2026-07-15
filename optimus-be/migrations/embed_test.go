902d25ff425bd652b890d7aa03312c353c817c16
logic <logic3579@duck.com>
2026-07-15 16:03:34 +0200

    feat(be/migrations): add 6 P4 assets tables + GORM models

--- optimus-be/migrations/embed_test.go
+++ optimus-be/migrations/embed_test.go
@@ -1,9 +1,11 @@
 package migrations
 
 import (
+	"math"
 	"strings"
 	"testing"
 
+	"github.com/pressly/goose/v3"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,4 +19,20 @@ func TestEmbedContainsAllMigrations(t *testing.T) {
 		}
 	}
 	require.GreaterOrEqual(t, sqlCount, 11, "expected at least 11 embedded migration files")
+}
+
+func TestEmbeddedMigrationsHaveUniqueVersions(t *testing.T) {
+	goose.SetBaseFS(FS)
+	t.Cleanup(func() { goose.SetBaseFS(nil) })
+
+	migrations, err := goose.CollectMigrations(".", 0, math.MaxInt64)
+	require.NoError(t, err)
+
+	var version21Count int
+	for _, migration := range migrations {
+		if migration.Version == 21 {
+			version21Count++
+		}
+	}
+	require.Equal(t, 1, version21Count, "migration version 21 must be discoverable exactly once")
 }
